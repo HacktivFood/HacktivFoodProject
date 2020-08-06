0646b9670b2b69d0dcaf24317444e4ee314dc857
suhardiman <suhardiman@suhardimandeMacBook-Air.local>
2020-08-06 22:56:00 +0800

    commit

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,14 +2,9 @@ const express = require('express')
 const router = express.Router()
 const usersRouter = require('./users.js').router
 const authentication = require('../middlewares/authentication.js').authentication
-const authorization = require('../middlewares/authorization.js').authorization
 
 router.use('/users', usersRouter)
 
 router.use(authentication)
-
-router.get('/', authorization, () => {
-    console.log('test')
-})
 
 module.exports = {router}

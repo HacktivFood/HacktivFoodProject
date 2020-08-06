05102d8cec18f9b205da51581c691820b36058f2
deshantt <dewishanty.workspace@gmail.com>
2020-08-07 05:40:47 +0700

    add api spoonacular to server -- still pending, fetch data to client

--- server/routers/index.js
+++ server/routers/index.js
@@ -5,10 +5,12 @@ const usersRouter = require('./users.js').router
 const authentication = require('../middlewares/authentication.js').authentication
 
 const RestaurantsController = require('../controllers/restaurantsController')
+const RecipeController = require('../controllers/recipeController')
 
 router.use('/users', usersRouter)
 
 // router.use(authentication)
 router.get('/restaurants', RestaurantsController.search)
+router.get('/recipes', RecipeController.getRecipe)
 
 module.exports = {router}
